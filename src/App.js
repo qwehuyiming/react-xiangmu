b8d21bbf8aab69cd0871d1e4cbb67a91efe0d01d
uksir <yeah126139163@163.com>
2019-06-21 10:22:21 +0800

    使用ui组件颜色

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import './App.css';
-
+import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
 function App() {
   return (
     <div className="App">
-      移动端项目
+      <Button>default</Button><WhiteSpace />
+      <Button disabled>default disabled</Button><WhiteSpace />
+
+      <Button type="primary">primary</Button><WhiteSpace />
+      <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
+
+      <Button type="warning">warning</Button><WhiteSpace />
+      <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
     </div>
   );
 }
