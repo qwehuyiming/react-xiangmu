32d57cb6fc3695bb0aa7c1bedbcf6a5cb2565fc7
uksir <yeah126139163@163.com>
2019-06-21 11:03:52 +0800

    使用tabbar组件成功

--- src/App.js
+++ src/App.js
@@ -1,19 +1,74 @@
 import React from 'react';
-import './App.css';
-import { Button, WhiteSpace, WingBlank } from 'antd-mobile';
-function App() {
-  return (
-    <div className="App">
-      <Button>default</Button><WhiteSpace />
-      <Button disabled>default disabled</Button><WhiteSpace />
+import './styles/App.css';
+import { TabBar } from 'antd-mobile';
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'redTab',
+      hidden: false,
+      fullScreen: false,
+    };
+  }
 
-      <Button type="primary">primary</Button><WhiteSpace />
-      <Button type="primary" disabled>primary disabled</Button><WhiteSpace />
 
-      <Button type="warning">warning</Button><WhiteSpace />
-      <Button type="warning" disabled>warning disabled</Button><WhiteSpace />
-    </div>
-  );
+  render() {
+    return (
+      <div style={ { position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white"
+        >
+          <TabBar.Item
+            title="首页"
+            key="Home"
+            icon={<span className="iconfont icon-home" /> }
+            selectedIcon={<span className="iconfont icon-home" /> }
+            selected={this.state.selectedTab === 'blueTab'}
+          
+            onPress={() => {
+              this.setState({
+                selectedTab: 'blueTab',
+              });
+            }}
+          >
+           1
+          </TabBar.Item>
+          <TabBar.Item
+            icon={ <span className="iconfont icon-gouwuche"  /> }
+            selectedIcon={ <span className="iconfont icon-gouwuche"  /> }
+            title="购物车"
+            key="Cart"
+            badge={1}
+            selected={this.state.selectedTab === 'redTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'redTab',
+              });
+            }}
+          >
+            2
+          </TabBar.Item>
+          <TabBar.Item
+            icon={<span className="iconfont icon-weibiaoti2fuzhi12" /> }
+            selectedIcon={ <span className="iconfont icon-weibiaoti2fuzhi12" /> }
+            title="我的"
+            key="Mine"
+            selected={this.state.selectedTab === 'greenTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'greenTab',
+              });
+            }}
+          >
+           3
+          </TabBar.Item>
+        </TabBar>
+      </div>
+    );
+  }
 }
+
 
 export default App;
