e337a1ec9b5d2d15333be729f7c3aef6eea73ab0
uksir <yeah126139163@163.com>
2019-06-22 10:15:20 +0800

    商品详情-点击返回

--- src/pages/GoodsDetail.js
+++ src/pages/GoodsDetail.js
@@ -1,8 +1,20 @@
 import React, { Component, Fragment } from 'react';
+import { NavBar, Icon } from 'antd-mobile';
 class GoodsDetail extends Component {
   render() {
+    // this.props.history.go(-1)
+    // console.log(this.props.history);
     return (
-      <Fragment>GoodsDetail</Fragment>
+      <Fragment>
+        {/* 导航栏 开始 */}
+        <NavBar
+          mode="dark"
+          icon={<Icon type="left" />}
+          // 点击图标 跳转会上一个页面
+          onLeftClick={() =>this.props.history.go(-1)}
+        >商品详情</NavBar>
+        {/* 导航栏 结束 */}
+      </Fragment>
     );
   }
 }
